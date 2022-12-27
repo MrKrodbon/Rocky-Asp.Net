e7b5f769e616adfff8d76e153f5d98bf1af1726b
eugene <sichi331@gmail.com>
2022-12-27 15:44:37 +0200

    Added Scaffold pages for sign in Users and Admins accounts

---
+++ CoursePractise/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CoursePractise.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoursePractise.Areas.Identity.IdentityHostingStartup))]
+namespace CoursePractise.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                
+            });
+        }
+    }
+}
