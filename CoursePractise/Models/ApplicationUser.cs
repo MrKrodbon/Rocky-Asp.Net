f33e6f7c64cd3988fa8c9762439b2be90437e2de
eugene <sichi331@gmail.com>
2022-12-27 15:49:41 +0200

    Added FullName Property for user

---
+++ CoursePractise/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoursePractise.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+
+
+    }
+}
